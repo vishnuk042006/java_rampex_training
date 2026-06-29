ac8399275d09c839243d7d45c1d41eb8b9adbc41
vishnuk042006 <vishnuk4906@gmail.com>
2026-06-29 22:04:38 +0530

    day 7 moveszero

---
+++ day01/moveszeros.java
@@ -0,0 +1,25 @@
+package day01;
+
+public class moveszeros  {
+    
+    public static void main(String[] args) {
+
+        int[] arr = {2, 0, 3, 0, 5};
+        int[] newArr = new int[arr.length];
+
+        int j = 0;
+
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                newArr[j] = arr[i];
+                j++;
+            }
+        }
+
+        
+        for (int num : newArr) {
+            System.out.print(num + " ");
+        }
+    }
+}
